d8a8f14ace8794693733cf4490f96805a7e35cae
mohanmepco <42240354+mohanmepco@users.noreply.github.com>
2018-10-30 08:33:31 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "default" {
+  name = "test"
+  machine_type = "n1-standard-1"
+  zone = "us-central1-b"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-6-v20181011"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
+provider "google" {
+  credentials = "${file("terraform-account.json")}"
+  project     = "cool-album-220315"
+  region      = "us-central1"
+}
